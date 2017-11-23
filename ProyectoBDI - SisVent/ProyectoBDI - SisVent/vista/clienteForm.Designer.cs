c2deead207ca172d9e895a29ea3e16fd62f5370f
RICARDOCHÁVEZJR\RicardoRené <rchavezrios@gmail.com>
2017-11-23 12:20:55 -0600

    MOSTRANDO Y AGREGANDO CLIENTES

--- ProyectoBDI - SisVent/ProyectoBDI - SisVent/vista/clienteForm.Designer.cs
+++ ProyectoBDI - SisVent/ProyectoBDI - SisVent/vista/clienteForm.Designer.cs
@@ -36,8 +36,6 @@
             this.txtDireccion = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.label4 = new System.Windows.Forms.Label();
             this.txtTel = new Bunifu.Framework.UI.BunifuMaterialTextbox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtID = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.label2 = new System.Windows.Forms.Label();
             this.txtEmail = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.label1 = new System.Windows.Forms.Label();
@@ -60,18 +58,17 @@
             this.bunifuCards1.Controls.Add(this.txtDireccion);
             this.bunifuCards1.Controls.Add(this.label4);
             this.bunifuCards1.Controls.Add(this.txtTel);
-            this.bunifuCards1.Controls.Add(this.label3);
-            this.bunifuCards1.Controls.Add(this.txtID);
             this.bunifuCards1.Controls.Add(this.label2);
             this.bunifuCards1.Controls.Add(this.txtEmail);
             this.bunifuCards1.Controls.Add(this.label1);
             this.bunifuCards1.Controls.Add(this.txtNombre);
             this.bunifuCards1.LeftSahddow = false;
-            this.bunifuCards1.Location = new System.Drawing.Point(-2, 40);
+            this.bunifuCards1.Location = new System.Drawing.Point(-3, 49);
+            this.bunifuCards1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bunifuCards1.Name = "bunifuCards1";
             this.bunifuCards1.RightSahddow = true;
             this.bunifuCards1.ShadowDepth = 20;
-            this.bunifuCards1.Size = new System.Drawing.Size(405, 500);
+            this.bunifuCards1.Size = new System.Drawing.Size(540, 615);
             this.bunifuCards1.TabIndex = 1;
             // 
             // cancelar
@@ -92,10 +89,10 @@
             this.cancelar.IdleFillColor = System.Drawing.Color.White;
             this.cancelar.IdleForecolor = System.Drawing.Color.RoyalBlue;
             this.cancelar.IdleLineColor = System.Drawing.Color.RoyalBlue;
-            this.cancelar.Location = new System.Drawing.Point(103, 438);
-            this.cancelar.Margin = new System.Windows.Forms.Padding(5);
+            this.cancelar.Location = new System.Drawing.Point(137, 539);
+            this.cancelar.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.cancelar.Name = "cancelar";
-            this.cancelar.Size = new System.Drawing.Size(120, 40);
+            this.cancelar.Size = new System.Drawing.Size(160, 49);
             this.cancelar.TabIndex = 12;
             this.cancelar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.cancelar.Click += new System.EventHandler(this.cancelar_Click);
@@ -118,20 +115,22 @@
             this.guardar.IdleFillColor = System.Drawing.Color.RoyalBlue;
             this.guardar.IdleForecolor = System.Drawing.Color.White;
             this.guardar.IdleLineColor = System.Drawing.Color.RoyalBlue;
-            this.guardar.Location = new System.Drawing.Point(256, 438);
-            this.guardar.Margin = new System.Windows.Forms.Padding(5);
+            this.guardar.Location = new System.Drawing.Point(341, 539);
+            this.guardar.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.guardar.Name = "guardar";
-            this.guardar.Size = new System.Drawing.Size(120, 40);
+            this.guardar.Size = new System.Drawing.Size(160, 49);
             this.guardar.TabIndex = 11;
             this.guardar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.guardar.Click += new System.EventHandler(this.guardar_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label5.Location = new System.Drawing.Point(31, 307);
+            this.label5.Location = new System.Drawing.Point(41, 378);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.Size = new System.Drawing.Size(67, 17);
             this.label5.TabIndex = 10;
             this.label5.Text = "Dirección";
             // 
@@ -147,10 +146,10 @@
             this.txtDireccion.LineIdleColor = System.Drawing.Color.RoyalBlue;
             this.txtDireccion.LineMouseHoverColor = System.Drawing.Color.RoyalBlue;
             this.txtDireccion.LineThickness = 3;
-            this.txtDireccion.Location = new System.Drawing.Point(25, 324);
-            this.txtDireccion.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDireccion.Location = new System.Drawing.Point(33, 399);
+            this.txtDireccion.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.txtDireccion.Name = "txtDireccion";
-            this.txtDireccion.Size = new System.Drawing.Size(351, 35);
+            this.txtDireccion.Size = new System.Drawing.Size(468, 43);
             this.txtDireccion.TabIndex = 9;
             this.txtDireccion.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             // 
@@ -158,9 +157,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label4.Location = new System.Drawing.Point(31, 225);
+            this.label4.Location = new System.Drawing.Point(41, 277);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.Size = new System.Drawing.Size(64, 17);
             this.label4.TabIndex = 8;
             this.label4.Text = "Teléfono";
             // 
@@ -176,50 +176,21 @@
             this.txtTel.LineIdleColor = System.Drawing.Color.RoyalBlue;
             this.txtTel.LineMouseHoverColor = System.Drawing.Color.RoyalBlue;
             this.txtTel.LineThickness = 3;
-            this.txtTel.Location = new System.Drawing.Point(25, 242);
-            this.txtTel.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTel.Location = new System.Drawing.Point(33, 298);
+            this.txtTel.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.txtTel.Name = "txtTel";
-            this.txtTel.Size = new System.Drawing.Size(157, 35);
+            this.txtTel.Size = new System.Drawing.Size(209, 43);
             this.txtTel.TabIndex = 7;
             this.txtTel.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label3.Location = new System.Drawing.Point(264, 60);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "ID ( * )";
-            // 
-            // txtID
-            // 
-            this.txtID.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.txtID.Enabled = false;
-            this.txtID.Font = new System.Drawing.Font("Century Gothic", 9.75F);
-            this.txtID.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.txtID.HintForeColor = System.Drawing.Color.Empty;
-            this.txtID.HintText = "";
-            this.txtID.isPassword = false;
-            this.txtID.LineFocusedColor = System.Drawing.Color.RoyalBlue;
-            this.txtID.LineIdleColor = System.Drawing.Color.RoyalBlue;
-            this.txtID.LineMouseHoverColor = System.Drawing.Color.RoyalBlue;
-            this.txtID.LineThickness = 3;
-            this.txtID.Location = new System.Drawing.Point(258, 77);
-            this.txtID.Margin = new System.Windows.Forms.Padding(4);
-            this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(118, 35);
-            this.txtID.TabIndex = 5;
-            this.txtID.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label2.Location = new System.Drawing.Point(31, 143);
+            this.label2.Location = new System.Drawing.Point(41, 176);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.Size = new System.Drawing.Size(42, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Email";
             // 
@@ -235,10 +206,10 @@
             this.txtEmail.LineIdleColor = System.Drawing.Color.RoyalBlue;
             this.txtEmail.LineMouseHoverColor = System.Drawing.Color.RoyalBlue;
             this.txtEmail.LineThickness = 3;
-            this.txtEmail.Location = new System.Drawing.Point(25, 160);
-            this.txtEmail.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEmail.Location = new System.Drawing.Point(33, 197);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(198, 35);
+            this.txtEmail.Size = new System.Drawing.Size(264, 43);
             this.txtEmail.TabIndex = 3;
             this.txtEmail.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             // 
@@ -246,9 +217,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label1.Location = new System.Drawing.Point(31, 60);
+            this.label1.Location = new System.Drawing.Point(41, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.Size = new System.Drawing.Size(85, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Nombre ( * )";
             // 
@@ -264,10 +236,10 @@
             this.txtNombre.LineIdleColor = System.Drawing.Color.RoyalBlue;
             this.txtNombre.LineMouseHoverColor = System.Drawing.Color.RoyalBlue;
             this.txtNombre.LineThickness = 3;
-            this.txtNombre.Location = new System.Drawing.Point(25, 77);
-            this.txtNombre.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombre.Location = new System.Drawing.Point(33, 95);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(198, 35);
+            this.txtNombre.Size = new System.Drawing.Size(264, 43);
             this.txtNombre.TabIndex = 1;
             this.txtNombre.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             // 
@@ -276,8 +248,9 @@
             this.panel1.BackColor = System.Drawing.Color.RoyalBlue;
             this.panel1.Controls.Add(this.tipoFormulario);
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(400, 44);
+            this.panel1.Size = new System.Drawing.Size(533, 54);
             this.panel1.TabIndex = 2;
             // 
             // tipoFormulario
@@ -285,20 +258,22 @@
             this.tipoFormulario.AutoSize = true;
             this.tipoFormulario.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tipoFormulario.ForeColor = System.Drawing.Color.White;
-            this.tipoFormulario.Location = new System.Drawing.Point(13, 11);
+            this.tipoFormulario.Location = new System.Drawing.Point(17, 14);
+            this.tipoFormulario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.tipoFormulario.Name = "tipoFormulario";
-            this.tipoFormulario.Size = new System.Drawing.Size(186, 22);
+            this.tipoFormulario.Size = new System.Drawing.Size(248, 30);
             this.tipoFormulario.TabIndex = 0;
             this.tipoFormulario.Text = "Información cliente";
             // 
             // clienteForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 539);
+            this.ClientSize = new System.Drawing.Size(533, 663);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.bunifuCards1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "clienteForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -319,8 +294,6 @@
         private Bunifu.Framework.UI.BunifuMaterialTextbox txtDireccion;
         private System.Windows.Forms.Label label4;
         private Bunifu.Framework.UI.BunifuMaterialTextbox txtTel;
-        private System.Windows.Forms.Label label3;
-        private Bunifu.Framework.UI.BunifuMaterialTextbox txtID;
         private System.Windows.Forms.Label label2;
         private Bunifu.Framework.UI.BunifuMaterialTextbox txtEmail;
         private System.Windows.Forms.Label label1;
--- ProyectoBDI - SisVent/ProyectoBDI - SisVent/vista/clienteForm.cs
+++ ProyectoBDI - SisVent/ProyectoBDI - SisVent/vista/clienteForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProyectoBDI___SisVent.data;
+using ProyectoBDI___SisVent.vista;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +14,7 @@ namespace ProyectoBDI___SisVent
 {
     public partial class clienteForm : Form
     {
+        EditCliente c = new EditCliente();
         public clienteForm()
         {
             InitializeComponent();
@@ -21,6 +24,24 @@ namespace ProyectoBDI___SisVent
 
         private void cancelar_Click(object sender, EventArgs e)
         {
+            this.Close();
+        }
+
+        private void guardar_Click(object sender, EventArgs e)
+        {
+            Cliente Client = new Cliente();
+            Client.C_Nombre = txtNombre.Text;
+            Client.C_Domicilio = txtDireccion.Text;
+            Client.C_Email = txtEmail.Text;
+            Client.C_Telefono = txtTel.Text;
+            try
+            {
+                c.InsertarCliente(Client);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR: Inserción fallida: "+ex.ToString());
+            }
             this.Close();
         }
     }
