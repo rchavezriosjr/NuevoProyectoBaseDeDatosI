a84072ab22f05bd1008a47995ef72e0c5dd0b94c
RICARDOCHÁVEZJR\RicardoRené <rchavezrios@gmail.com>
2017-12-04 00:17:07 -0600

    Reparacion de errores generados por merge de ramas

--- ProyectoBDI - SisVent/ProyectoBDI - SisVent/data/AddVenta.cs
+++ ProyectoBDI - SisVent/ProyectoBDI - SisVent/data/AddVenta.cs
@@ -12,7 +12,6 @@ namespace ProyectoBDI___SisVent.data
     {
         public void InsertarVenta(Venta vta)
         {
-<<<<<<< HEAD
             SqlConnection Con = new SqlConnection();
             try
             {
@@ -78,52 +77,17 @@ namespace ProyectoBDI___SisVent.data
                 Tot.SqlDbType = SqlDbType.Decimal;
                 Tot.Value = vta.MontoTotal;
                 cmd.Parameters.Add(Tot);
-=======
-            //SqlConnection Con = new SqlConnection();
-            //try
-            //{
-            //    Con.ConnectionString = Conexión.Cn;
-            //    Con.Open();
-            //    SqlCommand cmd = new SqlCommand();
-            //    cmd.Connection = Con;
-            //    cmd.CommandText = "AddVenta_Insertar";
-            //    cmd.CommandType = CommandType.StoredProcedure;
 
-            //    SqlParameter FVta = new SqlParameter();
-            //    FVta.ParameterName = "@FechaVenta";
-            //    FVta.SqlDbType = SqlDbType.DateTime;
-            //    FVta.Value = vta.FechaVenta;
-            //    cmd.Parameters.Add(FVta);
-
-            //    SqlParameter CId = new SqlParameter();
-            //    CId.ParameterName = "@ClienteId";
-            //    CId.SqlDbType = SqlDbType.Int;
-            //    CId.Value = vta.Cliente.C_Id;
-            //    cmd.Parameters.Add(CId);
-
-            //    SqlParameter MTot = new SqlParameter();
-            //    MTot.ParameterName = "@MontoTotal";
-            //    MTot.SqlDbType = SqlDbType.Decimal;
-            //    MTot.Value = vta.MontoTotal;
-            //    cmd.Parameters.Add(MTot);
-
-            //    SqlParameter UID = new SqlParameter();
-            //    UID.ParameterName = "@UserId";
-            //    UID.SqlDbType = SqlDbType.Int;
-            //    UID.Value = vta.UserId;
-            //    cmd.Parameters.Add(UID);
->>>>>>> 830e9ae5cc01e7db85483ac9b618eb616a2be282
-
-            //    cmd.ExecuteNonQuery();
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine("ERROR: Imposible insertar: " + ex.ToString());
-            //}
-            //finally
-            //{
-            //    if (Con.State == ConnectionState.Open) Con.Close();
-            //}
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: Imposible insertar: " + ex.ToString());
+            }
+            finally
+            {
+                if (Con.State == ConnectionState.Open) Con.Close();
+            }
         }
         public void ActualizarVenta(Venta vta)
         {
